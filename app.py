3ebe6b98484df4a3272a9e41d2cd0664570ab802
Joël Elunda <joel.elunda.01@gmail.com>
2020-03-01 19:32:35 +0100

    lines fonction

--- app.py
+++ app.py
@@ -3,3 +3,15 @@ from os import path
 from gtts import gTTS
 import speech_recognition as sr
 
+
+def lines(path):
+    # Une fonction déstiner à lire dans un fichier
+    # puis retourner les résultats dans une variable de
+    # chaine de caractères
+    file = open(path, "r")
+    lines = file.readlines()
+    for line in lines:
+        line += line
+    file.close()
+
+    return line
