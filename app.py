b7afa8416440a22edcedba0a260712043fc90a0b
Joël Elunda <joel.elunda.01@gmail.com>
2020-03-01 19:40:56 +0100

    reconnaireconnaissance vocale utilisant Google Speech Recognition

--- app.py
+++ app.py
@@ -32,3 +32,23 @@ with open("voices/voice.raw", "wb") as f:
 # Un fichier binaire
 with open("voices/voice.wav", "wb") as f:
     f.write(audio.get_wav_data())
+
+
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "voices/voice.wav")
+rec = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = rec.record(source)  # lecture entière du fichier audio
+
+    # reconnaissance vocale utilisant Google Speech Recognition
+
+try:
+    # utilisation d'une clé API par défaut pour le test
+    # si vous souhaitez utiliser une autre clé API, faites `rec.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # au lieu de `rec.recognize_google(audio)`
+    print("Google Speech Recognition penses que vous avez dit :  " + rec.recognize_google(audio))
+    with open("texts/translate/essai.txt", "w") as f:
+        f.write(rec.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition n'a pas pu comprendre l'audio")
+except sr.RequestError as e:
+    print("Requête échoué du service Google Speech Recognition : {0}".format(e))
